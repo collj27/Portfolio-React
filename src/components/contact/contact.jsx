eeaeed81f16673d04e79ed625208460003c505c8
James Coll <collj@bu.edu>
2022-12-16 12:20:37 -0500

    contact toast notifications

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,27 +1,45 @@
-import {useRef} from "react";
+import {useRef, useState} from "react";
 import emailjs from '@emailjs/browser';
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Row, Toast} from "react-bootstrap";
 import "./contact.css"
 
 function Contact() {
 
+    const [showToast, setShowToast] = useState(false);
+    const [toastMsg, setToastMsg] = useState("");
+    const [toastClass, setToastClass] = useState("");
+
+    const toggleShowToast = () => setShowToast(!showToast);
+
     const form = useRef();
+
 
     const sendEmail = (e) => {
         e.preventDefault(); // prevents the page from reloading when you hit “Send”
 
-        emailjs.sendForm('service_243igq8', 'template_jk9ky3h1', form.current, 'Yb6qKhAeEl4FShqPx')
+        emailjs.sendForm('service_243igq8', 'template_jk9ky3h', form.current, 'Yb6qKhAeEl4FShqPx')
             .then((result) => {
                 // show the user a success message
-               alert("success")
-            }, (error) => {
+                setToastMsg("Email sent successfully! I will respond as soon as possible.")
+                setToastClass("toast-success")
+                toggleShowToast()
+            }, () => {
                 // show the user an error
-                alert("error")
+                setToastMsg("Sorry, there was a problem sending the email. Please contact me on LinkedIn.")
+                setToastClass("toast-error")
+                toggleShowToast()
             });
     };
 
     return (
         <Container>
+            <Row className="justify-content-end">
+                <Col  xs={6} sm={5} md={4}>
+                    <Toast className={toastClass} show={showToast} onClose={toggleShowToast} delay={3000} autohide>
+                        <Toast.Body>{toastMsg}</Toast.Body>
+                    </Toast>
+                </Col>
+            </Row>
             <Row className="justify-content-center mt-5">
                 <Col>
                     <Row>
@@ -41,15 +59,19 @@ function Contact() {
                     <form ref={form} onSubmit={sendEmail}>
                         <Row>
                             <Col className="text-center">
-                                <input type="text" name="from_name" placeholder="Name" />
+                                <input type="text" name="from_name" placeholder="Name"/>
                             </Col>
                         </Row>
                         <Row className="mt-3">
                             <Col className="text-center">
-                                <input type="email" name="reply_to" placeholder="Email" />
+                                <input type="email" name="reply_to" placeholder="Email"/>
                             </Col>
                         </Row>
-
+                        <Row className="mt-3">
+                            <Col className="text-center">
+                                <input type="text" name="subject" placeholder="Subject"/>
+                            </Col>
+                        </Row>
                         <Row className="mt-3">
                             <Col className="text-center">
                                 <textarea name="message" placeholder="Message" rows="15" cols="40"/>
@@ -57,7 +79,7 @@ function Contact() {
                         </Row>
                         <Row>
                             <Col className="text-center">
-                                <input className="btn btn-primary" type="submit" value="Send" />
+                                <input className="btn btn-primary" type="submit" value="Send"/>
                             </Col>
                         </Row>
                     </form>
@@ -65,10 +87,7 @@ function Contact() {
             </Row>
 
 
-
         </Container>
-
-
 
 
     );
