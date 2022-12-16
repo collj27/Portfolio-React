201baeaff560065823e753fb5718448437d7ed34
James Coll <collj@bu.edu>
2022-12-16 08:31:29 -0500

    init commit

---
+++ src/components/skills.jsx
@@ -0,0 +1,48 @@
+import {Card, Col, Image, Nav, Navbar, Row} from "react-bootstrap";
+import aboutPic from "../dunks_nightcafe.jpg"
+import {faAngular, faPython, faJava, faReact,faSquareJs} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import postgresLogo from "../postgres_icon.png"
+
+function Skills() {
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>
+                    <span className="underlined-title">Tools I've Used</span>
+                </Card.Title>
+                <Row className="align-items-center mt-4">
+                    <Col className="text-center">
+                        <FontAwesomeIcon icon={faSquareJs} size="2xl" color="yellow"/>
+                        <span> Javascript</span>
+                    </Col>
+                    <Col className="text-center">
+                        <FontAwesomeIcon icon={faReact} size="2xl"  color="skyblue"/>
+                        <span>React</span>
+                    </Col>
+                    <Col className="text-center">
+                        <FontAwesomeIcon icon={faAngular} size="2xl" color="red"/>
+                        <span> Angular</span>
+                    </Col>
+                </Row>
+                <Row className="align-items-center mt-4">
+                    <Col className="text-center">
+                        <FontAwesomeIcon icon={faPython} size="2xl"/>
+                        <span> Python + Flask</span>
+                    </Col>
+                    <Col className="text-center">
+                        <FontAwesomeIcon icon={faJava} size="2xl"/>
+                        <span> Java +Spring Boot</span>
+                    </Col>
+                    <Col className="text-center">
+                        <Image src={postgresLogo} width="28px"></Image>
+                        <span> Postgres</span>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default Skills;
